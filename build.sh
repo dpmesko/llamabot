40b5d09c9661cd4154412df5144c747b1460aa13
Dan <dpmesko@gmail.com>
2025-03-12 00:40:35 -0400

    yet another filepath for the executable, i should delete this file

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -e
 stack build
-cp ~/llamabot/.stack-work/install/x86_64-linux-tinfo6/9b6cb93a50f935978de5337a9ac203bc719bfd9bc937bf2e822233eaa95e4614/9.0.2/bin/llamabot ./
+cp /home/dpmes/llamabot/.stack-work/install/x86_64-linux/5a73bdcfbe4d226bc9355406b65cdb1a0987447f63e9bf65625a4797d6ab1611/9.0.2/bin/llamabot ./
